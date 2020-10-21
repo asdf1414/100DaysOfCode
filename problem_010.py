1026e7db3c18f3c57cb930affd1dd41ad2ef8e49
asdf1414 <andreas.jokiel@hotmail.com>
2020-10-21 19:31:02 +0200

    bugfix for #10

--- problem_010.py
+++ problem_010.py
@@ -1,35 +1,31 @@
 # problem: https://mailchi.mp/59f844f71f1c/problem-10?e=6de8a670d8
 
 def search_pivot(lst):
-    # return if list has only one element
+    # return element if list has only one element
     if len(lst) == 1:
         return lst[0]
     # initial mid, start definement
-    end = len(lst) - 1
     start = 0
+    end = len(lst) - 1
 
     while True:
         # define middle element of current list part
-        mid = int((start + (end - start)) / 2)
+        mid = int(start + (end - start) / 2)
 
-        # check if current mind is smaller than left or bigger 
-        # than right neighbour
+        # check if current mid-num is smaller than start or bigger
+        # than end neighbour
         # if true: return current mid or mid+1 (found smallest)
         if lst[mid] < lst[mid - 1]:
             return lst[mid]
-        elif lst[mid] > lst[mid + 1]:
+        if lst[mid] > lst[mid + 1]:
             return lst[mid + 1]
-        
+
         # main algorithm logic. This comparsion checks if it should 
-        # search in the left or right half of the list
+        # search in the start or end half of the list
         if lst[mid] > lst[0]:
             start = mid + 1
         else:
             end = mid - 1
 
-    return lst[mid]
-
-
-# Driver code
-lst = [5,7,10,3,4]
+lst = [6,7,8,9,10,4]
 print(search_pivot(lst))
